ccf0068deff42fc1033b6912d1602fd84219085f
DDDBBBPPP <david.bellon.payer@gmail.com>
2024-11-25 11:10:16 +0100

    25/11/24

--- ejerciciotdd/EjercicioTDD.java
+++ ejerciciotdd/EjercicioTDD.java
@@ -9,9 +9,51 @@ public class EjercicioTDD {
         for(int i = 0;i<cadena.length();i++){
             cadenaInvertida = cadena.charAt(i) + cadenaInvertida;
         }
-        // TENGO QUE PONERR CADENAINVERTIDA Y PONERLO EN EL GOOGLDE DOCS
-        return "";
+
+        return cadenaInvertida;
     }
+
+    public static boolean palindromo(String cadena){
+        String cadenaInvertida = "";
+
+        for(int i = 0;i<cadena.length();i++){
+            cadenaInvertida = cadena.charAt(i) + cadenaInvertida;
+        }
+        
+        return(cadenaInvertida.equals(cadena));
+
+
+    }
+
+    public static int contarConsonantes(String cadena){
+        int contador = 0;
+        
+        for(int i = 0;i<cadena.length();i++){
+          
+            if(cadena.charAt(i) == 'a' || cadena.charAt(i) == 'e' || cadena.charAt(i) == 'i' || cadena.charAt(i) == 'o' || cadena.charAt(i) == 'u'){
+
+            }else{
+                contador +=1;
+            }
+        }
+        return contador;
+
+    }
+
+    public static boolean mayuscula(String cadena){
+        int i;
+        for(i = 0;i<cadena.length();i++){
+          if(Character.isUpperCase(cadena.charAt(i))){
+
+          }else{
+            i = (cadena.length() + 1);
+          }
+        }
+        return (i==cadena.length());
+
+    }
+    
     
 
 }
+;
--- ejerciciotdd/EjercicioTDDTest.java
+++ ejerciciotdd/EjercicioTDDTest.java
@@ -19,4 +19,156 @@ public class EjercicioTDDTest {
         Assert.assertEquals(resultadoEsperado, resultadoReal);
 
     }
+    @Test
+    public void testCadenaInvertida_2() {
+
+        // arrange
+        String texto = "Vamonos por ahi";
+        
+        String resultadoEsperado = "iha rop sonomaV";
+
+        // act
+        String resultadoReal = EjercicioTDD.invertirCadena(texto);
+        // assert
+        Assert.assertEquals(resultadoEsperado, resultadoReal);
+
+    }
+
+    @Test
+    public void testPalindromo_1() {
+
+        // arrange
+        String texto = "reconocer";
+
+        boolean resultadoEsperado = true;
+
+        // act
+        boolean resultadoReal = EjercicioTDD.palindromo(texto);
+        // assert
+        Assert.assertEquals(resultadoEsperado, resultadoReal);
+
+    }
+
+    @Test
+    public void testPalindromo_2() {
+
+        // arrange
+        String texto = "papypap";
+
+        boolean resultadoEsperado = true;
+
+        // act
+        boolean resultadoReal = EjercicioTDD.palindromo(texto);
+        // assert
+        Assert.assertEquals(resultadoEsperado, resultadoReal);
+
+    }
+
+    @Test
+    public void testPalindromo_3() {
+
+        // arrange
+        String texto = "hola";
+
+        boolean resultadoEsperado = false;
+
+        // act
+        boolean resultadoReal = EjercicioTDD.palindromo(texto);
+        // assert
+        Assert.assertEquals(resultadoEsperado, resultadoReal);
+
+    }
+
+    @Test
+    public void testContarConsonantes_1() {
+
+        // arrange
+        String texto = "hola";
+
+        int resultadoEsperado = 2;
+
+        // act
+        int resultadoReal = EjercicioTDD.contarConsonantes(texto);
+        // assert
+        Assert.assertEquals(resultadoEsperado, resultadoReal);
+
+    }
+
+    @Test
+    public void testContarConsonantes_2() {
+
+        // arrange
+        String texto = "tristes";
+
+        int resultadoEsperado = 5;
+
+        // act
+        int resultadoReal = EjercicioTDD.contarConsonantes(texto);
+        // assert
+        Assert.assertEquals(resultadoEsperado, resultadoReal);
+
+    }
+    @Test
+    public void testContarConsonantes_3() {
+
+        // arrange
+        String texto = "hola";
+
+        int resultadoEsperado = 5;
+
+        // act
+        int resultadoReal = EjercicioTDD.contarConsonantes(texto);
+        // assert
+        Assert.assertEquals(resultadoEsperado, resultadoReal);
+
+    }
+
+    @Test
+    public void testMayuscula_1() {
+
+        // arrange
+        String texto = "HOLA";
+
+        boolean resultadoEsperado = true;
+
+        // act
+        boolean resultadoReal = EjercicioTDD.mayuscula(texto);
+        // assert
+        Assert.assertEquals(resultadoEsperado, resultadoReal);
+
+    }
+
+    @Test
+    public void testMayuscula_2() {
+
+        // arrange
+        String texto = "VAYA";
+
+        boolean resultadoEsperado = true;
+
+        // act
+        boolean resultadoReal = EjercicioTDD.mayuscula(texto);
+        // assert
+        Assert.assertEquals(resultadoEsperado, resultadoReal);
+
+    }
+
+    @Test
+    public void testMayuscula_3() {
+
+        // arrange
+        String texto = "hola";
+
+        boolean resultadoEsperado = true;
+
+        // act
+        boolean resultadoReal = EjercicioTDD.mayuscula(texto);
+        // assert
+        Assert.assertEquals(resultadoEsperado, resultadoReal);
+
+    }
+
+
+
+
 }
